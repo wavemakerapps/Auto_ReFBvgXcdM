aac5354138b84173a9b9ba71e87ecb35d69a3c3c
root <root@61949b3d2f95>
2017-06-30 00:41:52 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_ReFBvgXcdM",
+  "homePage" : "Main",
+  "name" : "Auto_ReFBvgXcdM",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
